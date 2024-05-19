606a6da6d872fbe36f8d074aca8efef34e815d9d
MhsF021 <math.ferreira021@gmail.com>
2024-05-19 04:47:06 -0300

    Adicionando métodos para facilitar o código, ajustando a conexão com o banco e realizando os primeiros selects

--- BarberShop/src/main/java/com/sistema/barber/barbershop/BarberShop.java
+++ BarberShop/src/main/java/com/sistema/barber/barbershop/BarberShop.java
@@ -1,16 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.sistema.barber.barbershop;
 
-/**
- *
- * @author mathf
- */
+import com.sistema.barber.barbershop.helper.DatabaseHelper;
+import com.sistema.barber.barbershop.model.connection.ConnectionDB;
+import com.sistema.barber.barbershop.model.connection.ConnectionMySQL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class BarberShop {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws SQLException {
+        ConnectionDB connection = new ConnectionMySQL();
+        /*
+        System.out.println(connection.buscarConexao());
+        @anotacao: usado para saber se a conexão está sendo realizada com o banco de dados.
+         */
+        StringBuilder cNomeCliente = new StringBuilder();
+        cNomeCliente.append(" SELECT nome_cliente ");
+        cNomeCliente.append(" FROM barbershowdb.cliente "); //versão de teste
+
+        DatabaseHelper dbHelper = new DatabaseHelper(connection.buscarConexao());
+        ResultSet fNomeCliente = dbHelper.fetch(cNomeCliente.toString());
+
+        while (fNomeCliente.next()) {
+            String nomeCliente = fNomeCliente.getString(1);
+            System.out.println(nomeCliente);
+        }
     }
 }
